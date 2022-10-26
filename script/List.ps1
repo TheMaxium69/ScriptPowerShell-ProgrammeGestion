2dda53e1f9b27a3c78f256e1a93511db5c991f52
TheMaxium69 <63310746+TheMaxium69@users.noreply.github.com>
2022-10-26 17:33:39 +0200

    update

--- script/List.ps1
+++ script/List.ps1
@@ -1,3 +1,3 @@
-$arrayApp_Graph = "wallpaper32", "TranslucentTB", "RoundedTB", "BeWidgets", "DeepL", "Flow.Launcher", "ModernFlyoutsHost", "WSLTux", "PowerToys", "PhoneExperienceHost"
+$arrayApp_Graph = "wallpaper32", "TranslucentTB", "RoundedTB", "BeWidgets", "DeepL", "Flow.Launcher", "WSLTux", "PowerToys", "PhoneExperienceHost", "Rainmeter"
 
-$arrayApp_JustKill = "WhatApp", "msteams", "steam", "Spotify"
+$arrayApp_JustKill = "WhatApp", "msteams", "steam", "Spotify", "AnyDesk", "Everything", "OriginWebHelperService", "pservice", "ModernFlyoutsHost"
